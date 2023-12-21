3c9f71723747935d5c473c2057559afd4587c409
Shoeb Khan <>
2023-12-21 14:58:47 +0530

    error handling for default API call

--- FindMyIP/FindMyIP/Source/Module/IPAddress/Public/ViewModel/IPViewModel.swift
+++ FindMyIP/FindMyIP/Source/Module/IPAddress/Public/ViewModel/IPViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Alamofire
 
 public class IPViewModel: ObservableObject {
     // MARK: Published Property
@@ -15,14 +16,14 @@ public class IPViewModel: ObservableObject {
     private var networkManager: IPNetworkManagerDelegate
     
     // MARK: Initilize Property
-   public  init(networkManager: IPNetworkManagerDelegate = IPNetworkManager()) {
+    public init(networkManager: IPNetworkManagerDelegate = IPNetworkManager()) {
         self.networkManager = networkManager
     }
     
     // MARK: fetch  IPAddress with hard coded Model and URL
     public func fetchIPAddress() {
         self.errorMessage = ""
-        self.fetchDynamicResponseModel(url: "https://ipapi.co/json/", 
+        self.fetchDynamicResponseModel(url: "https://ipapi.co/json/",
                                        responseModel: IPAddressResponse.self) { [weak self] result in
             guard let self = self else {return}
             switch result {
@@ -30,19 +31,46 @@ public class IPViewModel: ObservableObject {
                 self.isLoading = false
                 self.ipAddress = response.ipAddressValue
             case .failure(let error):
-                self.errorMessage = error.localizedDescription
+                self.errorMessage = parseURLError(error as? AFError)
             }
         }
     }
     
     // MARK: fetch  IPAddress with User defined Model and URL
-    public  func fetchDynamicResponseModel<T: Decodable>(url: String, 
-                                                         responseModel:T.Type,
-                                                         completionHandler: @escaping (Result<T, Error>) -> Void) {
-        networkManager.fetchIPAddress(url: url, 
+    public func fetchDynamicResponseModel<T: Decodable>(url: String,
+                                                        responseModel:T.Type,
+                                                        completionHandler: @escaping (Result<T, Error>) -> Void) {
+        networkManager.fetchIPAddress(url: url,
                                       responseModel: responseModel) { result in
             completionHandler(result)
         }
     }
+}
+
+extension IPViewModel {
     
+    // MARK: Error handle
+    private func parseURLError(_ error: AFError?) -> String {
+        var errorMessage = "Unkown Error found"
+        if let afError = error {
+            switch afError {
+            case .sessionTaskFailed(let sessionError):
+                if let urlError = sessionError as? URLError {
+                    switch urlError.code {
+                    case .notConnectedToInternet:
+                        errorMessage = "Error: The Internet connection appears to be offline."
+                    default:
+                        errorMessage = "Error: \(urlError.localizedDescription)"
+                    }
+                } else {
+                    errorMessage = "Error: \(afError.localizedDescription)"
+                }
+            default:
+                errorMessage = "Error: \(afError.localizedDescription)"
+            }
+        } else {
+            errorMessage = "Error: \(error?.localizedDescription ?? errorMessage)"
+        }
+        return errorMessage
+    }
 }
--- FindMyIP/FindMyIPTests/IPViewModelTests.swift
+++ FindMyIP/FindMyIPTests/IPViewModelTests.swift
@@ -19,23 +19,6 @@ final class IPViewModelTests: XCTestCase {
         self.viewModel = nil
     }
     
-    func testFetchIPAddressFailure() {
-        let expectedResult = ""
-        self.viewModel = IPViewModel(networkManager:
-                                        MockIPNetworkManager(failureCase: true))
-        let expectation = self.expectation(description: "Fetch IP Address Failure")
-        self.viewModel?.fetchIPAddress()
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-    
-            XCTAssertEqual(self.viewModel?.ipAddress, expectedResult)
-            XCTAssertEqual(self.viewModel?.isLoading, true)
-            XCTAssertNotNil(self.viewModel?.errorMessage)
-            expectation.fulfill()
-        }
-        waitForExpectations(timeout: 10.0, handler: nil)
-    }
-    
     func testFetchIPAddressSuccess() {
         let expectedResult = "2402:e280:3d6e:1697:303b:210f:548c:13f4"
         
@@ -53,5 +36,21 @@ final class IPViewModelTests: XCTestCase {
         waitForExpectations(timeout: 10.0, handler: nil)
     }
     
+    func testFetchIPAddressFailure() {
+        let expectedResult = ""
+        self.viewModel = IPViewModel(networkManager:
+                                        MockIPNetworkManager(failureCase: true))
+        let expectation = self.expectation(description: "Fetch IP Address Failure")
+        self.viewModel?.fetchIPAddress()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
     
+            XCTAssertEqual(self.viewModel?.ipAddress, expectedResult)
+            XCTAssertEqual(self.viewModel?.isLoading, true)
+            XCTAssertNotNil(self.viewModel?.errorMessage)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 10.0, handler: nil)
+    }
+        
 }
