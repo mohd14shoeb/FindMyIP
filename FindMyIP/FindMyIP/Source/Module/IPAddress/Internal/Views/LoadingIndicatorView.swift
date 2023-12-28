9c3a80ba5a33085fb009d45b88bb4810846e1c80
Shoeb Khan <>
2023-12-28 10:58:02 +0530

    LoadingIndicator  keep it internal

---
+++ FindMyIP/FindMyIP/Source/Module/IPAddress/Internal/Views/LoadingIndicatorView.swift
@@ -0,0 +1,60 @@
+//
+//  LoadingIndicatorView.swift
+//  FindMyIP
+//
+//  Created by Shoeb Khan on 20/12/23.
+//
+
+import SwiftUI
+
+struct LoadingIndicatorView: View {
+    
+    private let isLoading: Bool
+    private let error: String?
+    private let retryAction: (() -> Void)?
+    
+    init(isLoading: Bool,
+         error: String?,
+         retryAction: ( () -> Void)?) {
+        self.isLoading = isLoading
+        self.error = error
+        self.retryAction = retryAction
+    }
+    
+    var body: some View {
+        Group {
+            if isLoading,
+               let error = error,
+               error.isEmpty {
+                VStack(alignment: .center) {
+                    ProgressView()
+                        .controlSize(.large)
+                        .progressViewStyle(CircularProgressViewStyle(tint: Color.gray))
+                }
+            } else if let error = error,
+                      !error.isEmpty {
+                HStack {
+                    VStack(spacing: 8) {
+                        Text(error).padding(.all, 10)
+                        if self.retryAction != nil {
+                            Button(action: self.retryAction!) {
+                                Text("Retry")
+                                    .font(. system(size: 16)).bold()
+                            }
+                            .foregroundColor(Color.blue)
+                            .buttonStyle(PlainButtonStyle())
+                        }
+                    }
+                    .font(. system(size: 16)).fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    LoadingIndicatorView(isLoading: true, error: "") {
+        print("hello")
+    }.border(.red)
+}
